7a5674659944fbbcfbeb65f991b70b696520f2d5
Franklin da Silva Batista <frsbatist@gmail.com>
2023-09-27 09:18:47 -0300

    Commit geral

--- Themes/Loja/login.php
+++ Themes/Loja/login.php
@@ -2,17 +2,17 @@
     require "Core/Ctdb.php";
 ?>
 
-<div class="modal_fade_blur"></div>
+<div class="modal_fade_blur" id="modal_fade_blur"></div>
 
-<fieldset class="modal_form_container radius container_login" id="container_login">
+<fieldset class="modal_form_container border radius container_login" id="container_login">
 
     <div class="header_form">
 
-        <h1 class=" color-green-dark font-text-extra">Login</h1>
+        <h1 class="title_form color-green-dark font-text-extra">Login</h1>
 
-        <div class="btn_close_login btn_red radius" id="btn_close_login">
-            <span class="line1"></span>
-            <span class="line2"></span>
+        <div class="btn_close_container btn_red radius" id="btn_close_login">
+            <span class="lines line1 bgcolor-white"></span>
+            <span class="lines line2 bgcolor-white"></span>
         </div>
 
     </div>
@@ -20,13 +20,13 @@
     <form action="" method="post" class="form_general form_login_user">
 
         <div class="input_email div_input  radius">
-            <input class="radius" type="email" class="user_email" name="user_email" id="user_email" required>
-            <label for="user_email">E-mail: </label>
+            <input class="input_data border radius" type="email" class="user_email" name="user_email" id="user_email" required>
+            <label class="label_input" for="user_email">E-mail: </label>
         </div>
 
         <div class="input_pass div_input  radius">
-            <input type="password" class="user_pass radius" name="user_pass" id="user_pass" required>
-            <label for="user_pass">Senha:</label>
+            <input type="password" class="input_data user_pass border radius" name="user_pass" id="user_pass" required>
+            <label class="label_input" for="user_pass">Senha:</label>
             <span class="showHide">
                 <i class="fa fa-eye"></i>
             </span>
@@ -38,8 +38,8 @@
         </div>
 
         <div class="remember">
-            <label for="remeber">Salvar Senha</label>
-            <input type="checkbox" id="remeber" name="remeber" value="Remember">
+            <input type="checkbox" id="remember" name="remember" value="remember">
+            <label class="label_checkbox" for="remember">Salvar Senha</label>
         </div>
 
         <div class="create" id="btn_open_register">
--- assets/Js/script.js
+++ assets/Js/script.js
@@ -1,51 +1,64 @@
 $(document).ready(function(){
-    // MODAL REDES SOCIAIS
-    $("#btn_socials_open").click(()=>{
-        $(".socials_container").css("height", "200px")
-        $("#btn_socials_open").css("display", "none")
-        $("#btn_socials_close").css("display", "block")
-    })
-    $("#btn_socials_close").click(()=>{
-        $(".socials_container").css("height", "0")
-        $("#btn_socials_close").css("display", "none")
-        $("#btn_socials_open").css("display", "block")
-    })
+    // // MODAL REDES SOCIAIS
+    // $("#btn_socials_open").click(()=>{
+    //     $(".socials_container").css("height", "200px")
+    //     $("#btn_socials_open").css("display", "none")
+    //     $("#btn_socials_close").css("display", "block")
+    // })
+    // $("#btn_socials_close").click(()=>{
+    //     $(".socials_container").css("height", "0")
+    //     $("#btn_socials_close").css("display", "none")
+    //     $("#btn_socials_open").css("display", "block")
+    // })
 
-    // MODAL LOGIN
-    $("#btn_open_login").click(()=>{
+    // // MODAL LOGIN
+    // $("#btn_open_login").click(()=>{
         
-        $(".container_login").css("opacity","1");
-        $(".container_login").css("pointer-events", "all");
-        $(".container_login").css("top", "50%");
-        $(".modal_fade_blur").css("opacity","1");
-        $(".modal_fade_blur").css("height","100%");
-    });
+    //     $(".container_login").css("opacity","1");
+    //     $(".container_login").css("pointer-events", "all");
+    //     $(".container_login").css("top", "50%");
+    //     $(".modal_fade_blur").css("opacity","1");
+    //     $(".modal_fade_blur").css("height","100%");
+    // });
 
+    // $("#btn_close_login").click(()=>{
+    //     $(".container_login").css("pointer-events", "none");
+    //     $(".container_login").css("top", "0");
+    //     $(".container_login").css("opacity","0");
+    //     $(".modal_fade_blur").css("height","0");
+    //     $(".modal_fade_blur").css("opacity","0");
+    // })
+    // $("#btn_open_register").click(()=>{
+    //     $(".container_login").css("top", "0");
+    //     $(".container_login").css("opacity","0");
+    //     $(".container_login").css("pointer-events","none");
+    //     $(".container_register").css("opacity","1");
+    //     $(".container_register").css("pointer-events","all");
+    //     $(".container_register").css("top","50%");
+
+    // })
+    // $("#btn_close_register").click(()=>{
+    //     $(".container_register").css("opacity","0");
+    //     $(".container_register").css("pointer-events","none");
+    //     $(".container_register").css("top","0");
+    //     $(".modal_fade_blur").css("height","0");
+    //     $(".modal_fade_blur").css("opacity","0");
+
+    // })
+
+    $("#btn_open_login").click(()=>{
+        $("#container_login").fadeIn(400)
+        // $("#container_login").css("display", "block")
+        $("#container_login").css("top", "30%")
+        $(".modal_fade_blur").css("height", "100%")
+        $(".modal_fade_blur").css("z-index", "5")
+    })
     $("#btn_close_login").click(()=>{
-        $(".container_login").css("pointer-events", "none");
-        $(".container_login").css("top", "0");
-        $(".container_login").css("opacity","0");
-        $(".modal_fade_blur").css("height","0");
-        $(".modal_fade_blur").css("opacity","0");
+        $("#container_login").fadeOut(300)
+        $("#container_login").css("top", "20%")
+        $(".modal_fade_blur").css("z-index", "-1")
+        // $(".modal_fade_blur").css("opacity", "0")
     })
-    $("#btn_open_register").click(()=>{
-        $(".container_login").css("top", "0");
-        $(".container_login").css("opacity","0");
-        $(".container_login").css("pointer-events","none");
-        $(".container_register").css("opacity","1");
-        $(".container_register").css("pointer-events","all");
-        $(".container_register").css("top","50%");
-
-    })
-    $("#btn_close_register").click(()=>{
-        $(".container_register").css("opacity","0");
-        $(".container_register").css("pointer-events","none");
-        $(".container_register").css("top","0");
-        $(".modal_fade_blur").css("height","0");
-        $(".modal_fade_blur").css("opacity","0");
-
-    })
-
     // $("#btn_open_register").click(()=>{
     //     $("#container_register").fadeIn(400)
     //     $("#container_register").css("top", "50%")
