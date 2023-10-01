6d9ae8efccbb9525264c81323f950fd13d0a9408
Franklin da Silva Batista <frsbatist@gmail.com>
2023-09-30 23:50:59 -0300

    CART.PHP
    23:50
    30/09/2023

--- Themes/Loja/cart.php
+++ Themes/Loja/cart.php
@@ -4,69 +4,83 @@
 
 <div class="modal_fade_blur"></div>
 
-<div class="container_cart">
+<div class="container_cart" id="container_cart">
+
 
     <table class="table">
+        <caption class="title_cart color-green-dark">
+            <h1>Finalizar compra</h1>
+        </caption>
+        <thead class="thead color-green-dark">
+            <tr>
+                <th scope="colgroup" colspan="2">
+                    <h3>Produtos</h3>
+                </th>
+                <th scope="col">
+                    <h3>Quant</h3>
+                </th>
+                <th scope="col">
+                    <h3>Valor</h3>
+                </th>
+                <th scope="col">
+                    <h3>Del</h3>
+                </th>
+            </tr>
+        </thead>
+        <?php for($i = 0; $i < 20; $i++): ?>
 
-        <?php for($i = 0; $i < 6; $i++): ?>
-
-        <tr>
-            <td class="td_img">
-                <img src="Images/Products/product.png" class="img_cart">
+        <tbody class="tbody">
+            <tr>
+                <td rowspan="3" class="td_img">
+                    <img src="images/products/product.png">
+                </td>
+                <td class="td_product">
+                    <h4>Tênis Adidas Sporting</h4>
+                </td>
+                <td rowspan="3" class="td_quantity">
+                    <input type="number" value="1">
+                </td>
+                <td rowspan="3" class="td_value">
+                    R$ 315,00
+                </td>
+                <td rowspan="3" class="td_delete">
+                    <div class="btn_delete btn_red radius">
+                        <span class="lines line1 bgcolor-white"></span>
+                        <span class="lines line2 bgcolor-white"></span>
+                    </div>
+                </td>
+            </tr>
+            <tr>
+                <td class="td_color">
+                    <p>Cor: <span>azul</span></p>
+                </td>
+            </tr>
+            <td class="td_size">
+                <p>Tamanho: <span>40</span></p>
             </td>
-
-            <td class="td_title">
-                <h2>Tênis Adidas Sport</h2>
-                <p>Cor: Azul - Tamanho 32</p>
-            </td>
-            <td class="td_quantity">
-                <form method="post" id="form_cart">
-                    <input type="number" id="input_cart" name="input_cart" value="1" class="border radius" />
-                </form>
-            </td>
-
-            <td class="td_price">
-                <p>R$ 320,00</p>
-            </td>
-
-            <td class="td_actions">
-                <p class="cart_actions">
-                    <a href="#" class="radius" id="delete_cart" data-id="<?= $i ?>">
-                    </a>
-                </p>
-            </td>
-        </tr>
+        </tbody>
         <?php endfor; ?>
 
-        <tr>
-            <td colspan="2">
-                <p class="text-left font-text-sub font-weight-medium">Cupom de Desconto:</p>
-
-                <form method="post" id="form_discount">
-                    <input type="text" id="input_discount" name="input_discount" class="radius">
-
-                    <button class="btn_search radius" name="btn_discount" id="btn_discount">
-                        <i class="fa fa-percent"></i> Aplicar Desconto</button>
-                </form>
-            </td>
-
-            <td colspan="3">
-                <p class="text-left font-text-sub font-weight-medium">Consulta do Custo do Frete:</p>
-                <form method="post" id="form_shipping">
-                    <input type="text" id="input_shipping" name="input_shipping" class="radius zipcode">
-
-                    <button class="btn_search radius" name="btn_shipping" id="btn_shipping">
-                        <i class="fa fa-truck"></i> Calcular</button>
-                </form>
-            </td>
-        </tr>
-
-        <tr>
-            <td colspan="5">
-                <p class="text-right font-text-medium font-weight-medium">Valor Total: R$ 1.280,00</p>
-            </td>
-        </tr>
-
+        <tfoot class="tfoot">
+            <tr>
+            </tr>
+            <tr>
+                <td colspan="2"><input type="text" class="font-medium border radius cupom" placeholder="Cupom de desconto"></td>
+                
+                <td rowspan="2">
+                    <h3>Total</h3>
+                </td>
+                <td colspan="2" rowspan="2" class="total_valor">R$ 315,00</td>
+            </tr>
+            <tr>
+                <td colspan="2" class="td_confirm">
+                    <button class="btn_orange border radius">Continuar a Compra</button>
+                    <button class="btn_green border radius">COMPRAR</button>
+                </td>
+                <td>
+                </td>
+            </tr>
+        </tfoot>
     </table>
 
 </div>
